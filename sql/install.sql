4b85c985ef74f8b339bb91163110c1c53c3f4267
Bas van den DIkkenberg <bas@dikkenberg.net>
2026-09-13 16:05:56 +0200

    Document database schema version

--- sql/install.sql
+++ sql/install.sql
@@ -1,5 +1,6 @@
 -- SPDX-License-Identifier: GPL-3.0-only
 -- Copyright (C) 2026 Bas van den Dikkenberg
+-- Activiteitenweger database schema version: 1
 
 -- phpMyAdmin SQL Dump
 -- version 5.2.2deb1+deb13u1
